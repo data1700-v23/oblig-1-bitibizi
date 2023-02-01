9572b95a0f285edabd7ce7d691ba353661a0e32b
bitibizi <hanbit199@gmail.com>
2023-02-01 14:21:57 +0100

    write objects in javascript and add div(for error messages) in html file.

---
+++ src/main/resources/static/Oblig1.js
@@ -0,0 +1,13 @@
+
+const film=document.getElementById("film").value
+const antall=document.getElementById("antall").value
+const fornavn=document.getElementById("fornavn").value
+const etternavn=document.getElementById("etternavn").value
+const tlf=document.getElementById("tlf").value
+const email=document.getElementById("email").value
+
+const list=[]
+
+kjop = () =>{
+    if(isNaN(antall)|| )
+}
